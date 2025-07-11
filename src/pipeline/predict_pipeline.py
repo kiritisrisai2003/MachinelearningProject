730919cbff96b0862aca34de699f9edbcfbadff2
kiritisrisai2003 <kiritisrisai@gmail.com>
2025-07-11 13:19:23 +0530

    predict_pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+
+            print("Before Loading")
+            print("Model path:", model_path)
+            print("Preprocessor path:", preprocessor_path)
+
+            model = load_object(file_path=model_path)
+            print("Model Loaded")
+
+            preprocessor = load_object(file_path=preprocessor_path)
+            print("Preprocessor Loaded")
+
+            data_scaled = preprocessor.transform(features)
+            print("Data transformed")
+
+            preds = model.predict(data_scaled)
+            print("Prediction done")
+            return preds
+        
+        except Exception as e:
+            print("🚨 Full Exception Trace:")
+            import traceback
+            traceback.print_exc()  # This will show the full error traceback
+            raise e
+
+            
+
+
+
+class CustomData:
+    def __init__(  self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
